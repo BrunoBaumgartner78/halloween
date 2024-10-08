cafea48cbc1dab34e6722da594528beb6362a856
BrunoBaumgartner78 <bruno@brainbloom.ch>
2024-10-08 17:36:41 +0200

    Updated files

--- src/menuList/MenuList.jsx
+++ src/menuList/MenuList.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import './menuList.css';
-import TigerDragon from '../TiegerDrachen/tiegerDrachen';
+import TigerDragon from "../TiegerDrachen/tiegerDrachen"
+
 const categories = [
   { id: 'z', title: 'Baumgartner Development', description: 'bruno@brainbloom.ch' },
-
+  { id: 'a', title: 'Dies ist ein Lizenzprototype', description: 'Anpassung ab 3000 CHF' },
 
 ];
 
@@ -17,10 +18,10 @@ const MenuList = () => {
           <div key={category.id} className="category-slide">
             <h1>{category.title}</h1>
             <p>{category.description}</p>
-           <TigerDragon /> 
+          
           </div>
         ))}
-     
+     <TigerDragon />
       </div>
     </div>
   );
